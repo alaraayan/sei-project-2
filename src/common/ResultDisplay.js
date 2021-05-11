7bfc68f5cbbcb24ca298b2d2163ef52e737cb2fd
alara <alaraayan@hotmail.com>
2021-05-11 15:00:34 +0100

    final-css

--- src/common/ResultDisplay.js
+++ src/common/ResultDisplay.js
@@ -20,21 +20,29 @@ function ResultDisplay() {
       <div className="columns"></div>
       <div className="field-result">
         <h1 className="result">You scored {finalResult} out of 10</h1>
-        {Object.entries(answers).map(([question, answer]) => {
-          return <div key={question} style={{ textAlign: 'center', marginBottom: '10px' }}>
-            <h6 dangerouslySetInnerHTML={ { __html: question } }></h6>
-            <p dangerouslySetInnerHTML={ { __html: answer } }></p>
-            
-          </div>
-
-        })}
+        
         <div className="buttons">
-          <button type="submit" className="button is-primary result is-fullwidth is-hovered" onClick={handleReplay}>
+          <button type="submit" className="button is-warning result is-fullwidth is-hovered" onClick={handleReplay}>
           Have Another Go
           </button>
           <button type="submit" className="button is-fullwidth is-danger result is-hovered" onClick={handleGoHome}>
           Play A Different Game
           </button>
+        </div>
+        <div className="correct-results">
+          <button className="button is-success result is-fullwidth is-hovered is-static " style={{ textAlign: 'center', marginBottom: '10px' }} disabled>Correct Answers:</button>
+          {Object.entries(answers).map(([question, answer]) => {
+            return <div key={question} style={{ textAlign: 'center', marginBottom: '10px' }}>
+              
+              <ul>
+                <h6 dangerouslySetInnerHTML={ { __html: question } }></h6>
+                <li dangerouslySetInnerHTML={ { __html: answer } }></li>
+              </ul>
+              
+              
+            </div>
+
+          })}
         </div>
       </div>
     </div>
